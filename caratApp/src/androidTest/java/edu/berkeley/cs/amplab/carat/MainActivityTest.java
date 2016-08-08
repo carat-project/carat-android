dc4d84fdcc4c6f605d7766cc43c484a66869ab19
Jonatan Hamberg <jonatan.hamberg@cs.helsinki.fi>
2016-08-08 13:37:55 +0300

    MainActivity: Unit tests for time strings

---
+++ caratApp/src/androidTest/java/edu/berkeley/cs/amplab/carat/MainActivityTest.java
@@ -0,0 +1,76 @@
+package edu.berkeley.cs.amplab.carat;
+
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.res.Resources;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.DisplayMetrics;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+import edu.berkeley.cs.amplab.carat.android.MainActivity;
+import edu.berkeley.cs.amplab.carat.android.R;
+
+//
+//  Created by Jonatan C Hamberg on 8.5.2016.
+//  Copyright © 2016 University of Helsinki. All rights reserved.
+//
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity activity;
+    private Instrumentation instrumentation;
+    private Context context;
+
+    private String UPDATED_NEVER;
+    private String UPDATED_NOW;
+
+    public MainActivityTest(){
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        instrumentation = getInstrumentation();
+        activity = getActivity();
+        context = instrumentation.getContext();
+        Resources resources = getInstrumentation().getTargetContext().getResources();
+
+        UPDATED_NEVER = resources.getString(R.string.neverupdated);
+        UPDATED_NOW = resources.getString(R.string.updatedjustnow);
+
+        // Use default locale when testing
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        android.content.res.Configuration configuration = resources.getConfiguration();
+        configuration.locale = new Locale("en");
+        resources.updateConfiguration(configuration, metrics);
+    }
+
+    public void testTimeStrings() {
+        assertEquals(activity.getTimeString(-1), UPDATED_NEVER);
+        assertEquals(activity.getTimeString(-1000), UPDATED_NEVER);
+        assertEquals(activity.getTimeString(-TimeUnit.DAYS.toMillis(10000)), UPDATED_NEVER);
+        assertEquals(activity.getTimeString(0), UPDATED_NOW);
+        assertEquals(activity.getTimeString(TimeUnit.SECONDS.toMillis(1)), UPDATED_NOW);
+        assertEquals(activity.getTimeString(TimeUnit.SECONDS.toMillis(59)), UPDATED_NOW);
+        assertEquals(activity.getTimeString(TimeUnit.SECONDS.toMillis(60)), "Updated a minute ago");
+        assertEquals(activity.getTimeString(TimeUnit.SECONDS.toMillis(119)), "Updated a minute ago");
+        assertEquals(activity.getTimeString(TimeUnit.SECONDS.toMillis(120)), "Updated 2 minutes ago");
+        assertEquals(activity.getTimeString(TimeUnit.SECONDS.toMillis(179)), "Updated 2 minutes ago");
+        assertEquals(activity.getTimeString(TimeUnit.MINUTES.toMillis(59)), "Updated 59 minutes ago");
+        assertEquals(activity.getTimeString(TimeUnit.MINUTES.toMillis(60)), "Updated an hour ago");
+        assertEquals(activity.getTimeString(TimeUnit.MINUTES.toMillis(119)), "Updated an hour ago");
+        assertEquals(activity.getTimeString(TimeUnit.MINUTES.toMillis(120)), "Updated 2 hours ago");
+        assertEquals(activity.getTimeString(TimeUnit.HOURS.toMillis(23)), "Updated 23 hours ago");
+        assertEquals(activity.getTimeString(TimeUnit.HOURS.toMillis(24)), "Updated a day ago");
+        assertEquals(activity.getTimeString(TimeUnit.HOURS.toMillis(47)), "Updated a day ago");
+        assertEquals(activity.getTimeString(TimeUnit.HOURS.toMillis(48)), "Updated 2 days ago");
+        assertEquals(activity.getTimeString(TimeUnit.DAYS.toMillis(6)), "Updated 6 days ago");
+        assertEquals(activity.getTimeString(TimeUnit.DAYS.toMillis(7)), "Updated a week ago");
+        assertEquals(activity.getTimeString(TimeUnit.DAYS.toMillis(13)), "Updated a week ago");
+        assertEquals(activity.getTimeString(TimeUnit.DAYS.toMillis(14)), "Updated 2 weeks ago");
+        assertEquals(activity.getTimeString(TimeUnit.DAYS.toMillis(100*7)), "Updated 100 weeks ago");
+        assertEquals(activity.getTimeString(TimeUnit.DAYS.toMillis(10000*7)), "Updated 10000 weeks ago");
+    }
+}
