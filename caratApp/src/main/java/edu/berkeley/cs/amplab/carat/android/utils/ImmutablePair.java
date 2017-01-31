7dd86ca9b086f4d37807a35762280a494636d1f6
Jonatan Hamberg <jonatan.hamberg@cs.helsinki.fi>
2017-01-31 04:39:08 +0200

    Full implementation for ImmutablePair

--- caratApp/src/main/java/edu/berkeley/cs/amplab/carat/android/utils/ImmutablePair.java
+++ caratApp/src/main/java/edu/berkeley/cs/amplab/carat/android/utils/ImmutablePair.java
@@ -6,9 +6,7 @@ import java.io.Serializable;
  * Created by Jonatan on 30.1.2017.
  */
 public class ImmutablePair<F extends Serializable, S extends Serializable> implements Serializable {
-    public F getFirst() {
-        return first;
-    }
+    private static final long serialVersionUID = -6388997905154127431L;
 
     private F first;
     private S second;
@@ -18,16 +16,29 @@ public class ImmutablePair<F extends Serializable, S extends Serializable> imple
         this.second = second;
     }
 
-    public void setFirst(F first) {
-        this.first = first;
+    public F getFirst() {
+        return first;
     }
 
     public S getSecond() {
         return second;
     }
 
-    public void setSecond(S second) {
-        this.second = second;
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof ImmutablePair)){
+            return false;
+        }
+        ImmutablePair<?, ?> pair = (ImmutablePair<?, ?>) obj;
+        boolean eq1 = first == pair.first || first.equals(pair.first);
+        boolean eq2 = second == pair.second || second.equals(pair.second);
+        return eq1 && eq2;
     }
 
+    @Override
+    public int hashCode() {
+        int h1 = first == null ? 0 : first.hashCode();
+        int h2 = second == null ? 0 : second.hashCode();
+        return h1 ^ h2;
+    }
 }
