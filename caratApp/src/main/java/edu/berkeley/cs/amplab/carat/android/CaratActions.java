bc274ec38e65bf8fa833c9163ce5173f9727eac4
Jonatan Hamberg <jonatan.hamberg@cs.helsinki.fi>
2017-11-21 23:42:54 +0200

    charging: update fragment on broadcasts

--- caratApp/src/main/java/edu/berkeley/cs/amplab/carat/android/CaratActions.java
+++ caratApp/src/main/java/edu/berkeley/cs/amplab/carat/android/CaratActions.java
@@ -10,6 +10,8 @@ public class CaratActions {
     public static final String CHECK_SCHEDULE = ACTION_BASE + ".CHECK_SCHEDULE";
     public static final String RAPID_SAMPLING = ACTION_BASE + ".RAPID_SAMPLING";
     public static final String CHARGING_ANOMALY = ACTION_BASE + ".CHARGING_ANOMALY";
+    public static final String CHARGING_NEW = ACTION_BASE + ".CHARGING_NEW";
+    public static final String CHARGING_UPDATE = ACTION_BASE + ".CHARGING_UPDATE";
     public static final String STOP_RAPID_SAMPLING = ACTION_BASE + ".STOP_RAPID_SAMPLING";
     public static final String RAPID_SAMPLER_DYING = ACTION_BASE + ".RAPID_SAMPLER_DYING";
 }
--- caratApp/src/main/java/edu/berkeley/cs/amplab/carat/android/fragments/ChargingFragment.java
+++ caratApp/src/main/java/edu/berkeley/cs/amplab/carat/android/fragments/ChargingFragment.java
@@ -1,7 +1,10 @@
 package edu.berkeley.cs.amplab.carat.android.fragments;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.databinding.DataBindingUtil;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -11,6 +14,7 @@ import android.view.ViewGroup;
 
 import java.util.SortedMap;
 
+import edu.berkeley.cs.amplab.carat.android.CaratActions;
 import edu.berkeley.cs.amplab.carat.android.R;
 import edu.berkeley.cs.amplab.carat.android.databinding.FragmentChargingBinding;
 import edu.berkeley.cs.amplab.carat.android.models.ChargingSession;
@@ -20,26 +24,48 @@ import edu.berkeley.cs.amplab.carat.android.utils.Logger;
 /**
  * Created by Jonatan on 21.11.2017.
  */
-
 public class ChargingFragment extends Fragment {
     private static final String TAG = ChargingFragment.class.getSimpleName();
     private FragmentChargingBinding binding;
     private ChargingSessionManager sessionManager;
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+              switch(intent.getAction()){
+                  case CaratActions.CHARGING_NEW:
+                  case CaratActions.CHARGING_UPDATE:
+                      setChargingSessions(sessionManager.getSavedSessions());
+              }
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_charging, container, false);
         sessionManager = ChargingSessionManager.getInstance();
-        new Handler().post(() -> setChargingSessions(sessionManager.getSavedSessions()));
+        new Handler().post(() -> {
+            setChargingSessions(sessionManager.getSavedSessions());
+        });
+        getContext().registerReceiver(receiver, new IntentFilter(CaratActions.CHARGING_NEW));
+        getContext().registerReceiver(receiver, new IntentFilter(CaratActions.CHARGING_UPDATE));
         return binding.getRoot();
     }
 
     private void setChargingSessions(SortedMap<Long, ChargingSession> sessions){
         Logger.d(TAG, "Got " + sessions.size() + " sessions");
         StringBuilder sb = new StringBuilder();
+        boolean captureFirst = true;
         for(Long timestamp : sessions.keySet()){
+            if(captureFirst){
+                captureFirst = false;
+            }
             sb.append(sessions.get(timestamp)).append("\n");
         }
         binding.chargingDebug.setText(sb.toString());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 }
