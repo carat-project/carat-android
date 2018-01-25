44456b4de71de546926dcbf4a63eca308b986bbf
Jonatan Hamberg <jonatan.hamberg@cs.helsinki.fi>
2018-01-26 00:18:06 +0200

    Make internet dialog buttons match the theme

--- caratApp/src/main/java/edu/berkeley/cs/amplab/carat/android/fragments/EnableInternetDialogFragment.java
+++ caratApp/src/main/java/edu/berkeley/cs/amplab/carat/android/fragments/EnableInternetDialogFragment.java
@@ -5,6 +5,7 @@ import edu.berkeley.cs.amplab.carat.android.R;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 
@@ -14,17 +15,20 @@ public class EnableInternetDialogFragment extends DialogFragment {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage(R.string.dialog_text)
-               .setPositiveButton(R.string.dialog_enable, new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int id) {
-                       ((MainActivity) getActivity()).GoToWifiScreen();
-                   }
+               .setPositiveButton(R.string.dialog_enable, (dialog, id) -> {
+                   MainActivity mainActivity = (MainActivity) getActivity();
+                   mainActivity.GoToWifiScreen();
                })
-               .setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int id) {
-                	   EnableInternetDialogFragment.this.getDialog().cancel();
-                   }
+               .setNegativeButton(R.string.dialog_cancel, (dialog, id) -> {
+                   EnableInternetDialogFragment.this.getDialog().cancel();
                });
+
         // Create the AlertDialog object and return it
-        return builder.create();
+        AlertDialog dialog = builder.create();
+        dialog.setOnShowListener(dialogInterface -> {
+            dialog.getButton(Dialog.BUTTON_NEGATIVE).setTextColor(Color.rgb(248, 176, 58));
+            dialog.getButton(Dialog.BUTTON_POSITIVE).setTextColor(Color.rgb(248, 176, 58));
+        });
+        return dialog;
     }
 }
