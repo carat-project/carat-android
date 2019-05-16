cbf721878bdaeb492f0925be2df1303047858cd6
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2019-05-16 23:04:19 +0800

    Request permissions after EULA accepted. For old users, request perms if not granted every time the app would refresh.

--- caratApp/src/main/java/edu/berkeley/cs/amplab/carat/android/MainActivity.java
+++ caratApp/src/main/java/edu/berkeley/cs/amplab/carat/android/MainActivity.java
@@ -1,10 +1,12 @@
 package edu.berkeley.cs.amplab.carat.android;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -12,9 +14,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -258,12 +263,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         acceptedEula = p.getBoolean(getResources().getString(R.string.save_accept_eula), false);
         if(acceptedEula){
             Logger.d(Constants.SF, "EULA is accepted, waiting to access scheduleRefresh block");
-            synchronized (this){
-                Logger.d(Constants.SF, "In scheduleRefresh block");
-                scheduleRefresh(Constants.FRESHNESS_TIMEOUT);
+            if (checkPermissions()) {
+                synchronized (this) {
+                    Logger.d(Constants.SF, "In scheduleRefresh block");
+                    scheduleRefresh(Constants.FRESHNESS_TIMEOUT);
+                }
             }
         }
         setValues();
+    }
+
+    String[] permissions = new String[]{
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.ACCESS_COARSE_LOCATION};
+
+    private static final int MULTIPLE_PERMISSIONS = 32;
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MULTIPLE_PERMISSIONS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    synchronized (this) {
+                        Logger.d(Constants.SF, "In scheduleRefresh block");
+                        scheduleRefresh(Constants.FRESHNESS_TIMEOUT);
+                    }
+                    // all permissions are granted.
+                } else {
+                    //permissions missing, do nothing.
+
+                }
+                return;
+            }
+        }
+    }
+
+    private boolean checkPermissions() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(MainActivity.this, p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -345,6 +394,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
     }
 
     @Override
