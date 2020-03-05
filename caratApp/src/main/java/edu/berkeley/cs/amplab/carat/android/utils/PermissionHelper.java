e30ee4d1decd1e8b2f4a55c9337b5ad135524e6c
Jonatan Hamberg <jonatan.hamberg@outlook.com>
2020-03-05 20:28:15 +0200

    Create a helper for prompting all missing permissions
    This class automatically reads the permissions declared in the manifest
    and requests those that are missing the PERMISSION_GRANTED flag. Note
    that in the future location might need to be requested separately.

---
+++ caratApp/src/main/java/edu/berkeley/cs/amplab/carat/android/utils/PermissionHelper.java
@@ -0,0 +1,59 @@
+package edu.berkeley.cs.amplab.carat.android.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PermissionHelper {
+    private static final String TAG = PermissionHelper.class.getSimpleName();
+
+    public static void promptMissingPermissions(Activity activity) {
+        Context context = activity.getApplicationContext();
+        String[] permissions = getPackagePermissions(context);
+        if (permissions == null || permissions.length <= 0) {
+            Logger.e(TAG, context.getPackageName() + " had empty or null permissions!");
+            return;
+        }
+        String[] missing = filterMissingPermissions(activity, permissions);
+        ActivityCompat.requestPermissions(activity, missing, 123);
+    }
+
+    private static String[] filterMissingPermissions(Context context, String[] permissions) {
+        List<String> missing = new LinkedList<>();
+        for (String permission : permissions) {
+            try {
+                int code = ActivityCompat.checkSelfPermission(context, permission);
+                if (code != PackageManager.PERMISSION_GRANTED) {
+                    missing.add(permission);
+                }
+            } catch (IllegalArgumentException e) {
+                Logger.e(TAG, "Permission " + permission + " does not exist!", e);
+            }
+        }
+        return missing.toArray(new String[0]);
+    }
+
+    private static String[] getPackagePermissions(Context context) {
+        PackageManager pm = context.getPackageManager();
+        if (pm == null) {
+            return null;
+        }
+
+        String pkg = context.getPackageName();
+        try {
+            PackageInfo info = pm.getPackageInfo(pkg, PackageManager.GET_PERMISSIONS);
+            if (info == null) {
+                return null;
+            }
+            return info.requestedPermissions;
+        } catch (PackageManager.NameNotFoundException e) {
+            Logger.e(TAG, "Failed finding permissions for package " + pkg, e);
+            return null;
+        }
+    }
+}
